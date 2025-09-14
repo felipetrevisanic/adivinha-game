edc73aa6920765ecde17fd066d0a5698bdcbe3bb
Felipe Trevisani <felipetcardosoo@gmail.com>
2025-09-14 19:22:00 -0300

    finish initial react studies

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,128 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Header } from './components/Header'
+import { Tip } from './components/Tip'
+import { Letter } from './components/Letter'
+import { Input } from './components/Input'
+import { Button } from './components/Button'
+import { LetterUsed } from './components/LettersUsed'
+import type {LetterUsedProps} from './components/LettersUsed'
+import { WORDS } from './utils/words'
+import type { Challenge } from './utils/words'
+import { useEffect, useState } from 'react'
+import styles from './app.module.css'
+import "./global.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [score, setScore] = useState(0)
+  const [letter, setLetter] = useState("")
+  const [lettersUsed, setLettersUsed] = useState<LetterUsedProps[]>([])
+  const [challenge, setChallenge] = useState<Challenge | null>(null)
+
+  const ATTEMPT_LIMIT = (challenge?.word.length ?? 0) + 5
+  
+  function handleRestartGame(){
+    const isConfirmed = window.confirm("Você tem certeza?")
+
+    if(isConfirmed){
+      startGame()
+    }
+  }
+
+  function startGame(){
+    const index = Math.floor(Math.random() * WORDS.length)
+    const randomWord = WORDS[index]
+    setChallenge(randomWord)
+
+    setScore(0)
+    setLetter("")
+    setLettersUsed([])
+  }
+
+  function handleConfirm(){
+    if(!challenge){
+      console.log("erer")
+    }
+
+    if(!letter.trim()){
+      return alert('digite uma letra')
+    }
+
+    const value = letter.toUpperCase()
+    const exists = lettersUsed.find((used) => used.value.toUpperCase() === value)
+
+    if(exists){
+      setLetter("")
+      return alert('Você já utilizou a letra:' + value)
+    }
+
+    const hits = challenge?.word.toUpperCase().split("").filter((char) => char===value).length
+
+
+    
+    const correct = (hits ?? 0) > 0
+    const currentScore = score + (hits ?? 0)
+    
+    setLettersUsed((prevState) => [...prevState, {value, correct}])
+    setScore(currentScore)
+    setLetter("")
+  }
+
+  function endGame(message: string) {
+    alert(message)
+  }
+
+  useEffect(() => {
+    startGame()
+  }, [])
+
+  useEffect(() => {
+    if(!challenge){
+      return
+    }
+
+    setTimeout(() => {
+      if(score === challenge.word.length) {
+        return endGame("Parabéns você descobriu a palavra")
+      } 
+
+      console.log(ATTEMPT_LIMIT , LetterUsed.length)
+      if(lettersUsed.length === ATTEMPT_LIMIT){
+        return endGame("Que pena, você usou todas as tentativas")
+      }
+    })  
+
+  }, [score, lettersUsed.length])
+
+  if(!challenge){
+    return
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className={styles.container}>
+      <main>
+        <Header current={lettersUsed.length} max={ATTEMPT_LIMIT} onRestart={handleRestartGame} />
+        <Tip tip={challenge.tip}/>
+
+        <div className={styles.word}>
+          {
+            challenge.word.split("").map((letter, index) => {
+              const letterUsed = lettersUsed.find((used) => used.value.toUpperCase() === letter.toUpperCase())
+
+              return (<Letter key={index} value={letterUsed?.value} color={letterUsed?.correct ? "correct" : "default"}/>)
+            })
+          }
+        </div>
+
+        <h4>Palpite</h4>
+
+        <div className={styles.gues}>
+          <Input autoFocus maxLength={1} placeholder="?" onChange={(e: any) => setLetter(e.target.value)} value={letter}/>
+          <Button title='Confirmar' onClick={handleConfirm}/>
+        </div>
+
+        <LetterUsed data={lettersUsed}/>
+
+      </main>
+    </div>
   )
 }
 
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./styles.module.css"
+
+type Props = React.ComponentProps<"input"> & {
+
+}
+
+export function Input({...rest}){
+    return(
+        <input type="text" className={styles.input} {...rest}/>
+    )
+}
+
